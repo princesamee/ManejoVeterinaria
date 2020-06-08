c693c3e7cab156f25bded0c3ddcc40606e1c0fc4
princesamee <mbrassesco@dc.uba.ar>
2020-06-07 21:32:36 -0300

    Primera Visualización de Productos en Stock con su web html básica

--- LibroDiario/views.py
+++ LibroDiario/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Producto
+
+def post_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'LibroDiario/post_productos.html', {'productos': productos})
