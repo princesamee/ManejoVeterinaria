5ffb89b848a6da8a0577a306dd97086f464c08fc
princesamee <mbrassesco@dc.uba.ar>
2020-06-13 20:27:10 -0300

    Agrego medio de pago. Default Mercado Pago

--- LibroDiario/models.py
+++ LibroDiario/models.py
@@ -22,13 +22,20 @@ class Producto(models.Model):
         return self.nombre
 
 
+class MedioDePago(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
 class MovimientoLibro(models.Model):
     fecha_movimiento = models.DateTimeField(default=timezone.now)
     producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING)
     cantidad = models.PositiveIntegerField(help_text="Sólo números positivos")
     egreso = models.BooleanField(default=True)
+    medio_de_pago = models.ForeignKey(MedioDePago, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return (
-                   "Egreso" if self.egreso else "Ingreso") + " de " + self.producto.nombre + ", fecha: " + str(
+        return ("Venta" if self.egreso else "Ingreso") + " de " + self.producto.nombre + ", fecha: " + str(
             self.fecha_movimiento)
